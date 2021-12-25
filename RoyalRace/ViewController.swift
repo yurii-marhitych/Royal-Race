e4882ff85f74a0a34b38e348b1bec5e1c4115c46
Yurii Marhitych <marhitych.y@icloud.com>
2021-12-25 18:01:32 +0200

    Initial Commit

---
+++ RoyalRace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoyalRace
+//
+//  Created by Юра Маргітич on 25.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
